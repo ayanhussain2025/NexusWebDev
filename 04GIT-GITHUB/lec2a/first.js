2edd9ebdb7ecfd7884ff72be06969c46b65457c8
Ayan Hussain <youcandoit950@gmail.com>
2025-07-16 18:41:58 +0530

    uploading webdev folder

---
+++ 04GIT-GITHUB/lec2a/first.js
@@ -0,0 +1,12 @@
+const name = "Ayan";
+const age = 22;
+const balance = 500;
+
+function updateBalance()
+{
+    balance+=20;
+}
+
+updateBalance();
+
+console.log(balance);
---
+++ 04GIT-GITHUB/lec3/first.js
@@ -0,0 +1,18 @@
+const name = "Ayan";
+const age = 22;
+const balance = 500;
+
+function updateBalance()
+{
+    balance+=20;
+}
+
+updateBalance();
+
+console.log(balance);
+
+console.log("latest changes");
+
+console.log("Bug Fixed by me");
+
+console.log("git mein commit kyu nahi horha h");
