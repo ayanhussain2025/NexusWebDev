7e9f018ce68a7265e66f609a7599a91b040c2faa
Ayan Hussain <youcandoit950@gmail.com>
2025-07-16 20:18:19 +0530

    number

---
+++ 03js/day07/index.js
@@ -0,0 +1,12 @@
+let num1=308;
+let num2=new Number(308);
+let num3=new Number(308);
+console.log(num1==num2);
+console.log(num2==num3);
+
+let num = 284.965;
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));
+console.log(num.toExponential(0));
+console.log(num.toString());
+console.log(num.valueOf());
