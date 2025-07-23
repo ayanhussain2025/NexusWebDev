72c46ae1da723b6bad3be9aa501ad8acc3064c34
Ayan Hussain <codewithayanhn@gmail.com>
2025-07-23 11:31:40 +0530

    2nd way of creating function via variable

---
+++ 03js/lec12/pg2.js
@@ -0,0 +1,15 @@
+//2nd way of creating function : we can also store function in a variable
+
+let greet = function()
+{
+    console.log("hello guys kaise ho");
+    console.log("badhiya hu tu bta");
+}
+
+greet();
+
+const multiply = function(number1,number2){
+    return number1*number2;
+}
+
+console.log(multiply(2,3));
