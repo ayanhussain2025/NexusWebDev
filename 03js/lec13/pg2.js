35539c43d7dd7c98e4cba20842d8e5df253d6d1d
Ayan Hussain <codewithayanhn@gmail.com>
2025-07-28 05:24:21 +0530

    how to print values of object using loop

--- 03js/lec13/pg2.js
+++ 03js/lec13/pg2.js
@@ -33,3 +33,34 @@ do{
     console.log(b);
     b++;
 }while(b<=0); //it run loop one time even if the condition is false
+
+// Q1. How to print element of an array
+
+let arr = [ 20 , 30 , 40 ,70 ,43];
+for(let i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
+
+//Q2. How to print element of an object
+
+const user={
+    name : "Ayan Hussain",
+    age : 22,
+    gender : "Male",
+    balance : 400
+}
+
+
+const values = Object.values(user);
+for(let i=0;i<values.length;i++)
+{
+    console.log(values);
+}
+
+const keys = Object.keys(user);
+for(let i=0;i<values.length;i++)
+{
+    console.log(keys[i],user[keys[i]]);
+}
+
